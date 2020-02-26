c48d9a61544a3b71d6c23eb40a15cbbf542a1e2b
Nyayic Fanny <chrosbiefannie@gmail.com>
2020-02-26 15:51:00 +0300

    Added coveralls configuration

--- test/factorialTest.js
+++ test/factorialTest.js
@@ -6,9 +6,23 @@ function factorial(x) {
 }
 
 describe('multiply by itself', function(){
+
     it('it should return 120 when value is 5', function(){
         assert.equal(factorial(2),120);
-       
     });
+
+    it('factorial of 1 should be one', function() {
+      assert.equal(1, factorial(1));
+    });
+    it('factorial of 10 should be 3628800', function() {
+      assert.equal(3628800, factorial(10));
+    });
+    it('factorial of n should not be negative', function() {
+      assert.equal(NaN, factorial(-10));
+    });
+    it('factorial of n should not be more than 69', function() {
+      assert.equal(NaN, factorial(70));
+    });
+
 });
 
